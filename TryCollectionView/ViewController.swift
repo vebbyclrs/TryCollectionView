37ddb52fd463b977739220b41bc6de258ee97a8b
Vebby Clarissa <vebbyclarissa@gmail.com>
2019-07-04 15:34:33 +0700

    Initial Commit

---
+++ TryCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TryCollectionView
+//
+//  Created by Vebby Clarissa on 04/07/19.
+//  Copyright © 2019 Vebby Clarissa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
